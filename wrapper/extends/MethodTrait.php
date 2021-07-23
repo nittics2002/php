c8be77a986668db5d440dcecb475e269d812fb9b
nittics1991 <bunpabcom@gmail.com>
2021-07-23 10:18:59 +0900

    wrapper/array 拡張作成中

---
+++ wrapper/extends/MethodTrait.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+*   DefineMethodTrait
+*
+*   @version 210716
+*/
+
+declare(strict_types=1);
+
+namespace Concerto\wrapper\extend;
+
+trait DefineMethodTrait
+{
+    /**
+    *   nth
+    *
+    *   @param int $position
+    *   @return mixed
+    */
+    public function nth(
+        int $position
+    //): mixed {
+    ) {
+        $array = $this->toArray();
+        $counter = 0;
+
+        while ($counter < $position) {
+          if ($position > 0) {
+
+            next($array);
+          } else {
+            prev($array);
+          }
+      }
+      return current($array);
+    }
+
+    /**
+    *   first
+    *
+    *   @return mixed
+    */
+    public function first(
+    //): mixed {
+    ) {
+      return static::nth(0);
+    }
+    
+    /**
+    *   last
+    *
+    *   @return mixed
+    */
+    public function last(
+    //): mixed {
+    ) {
+      return static::nth(-1);
+    }
+
+    /**
+    *  insert 
+    *
+    *   @param int $position
+    *   @param mixed $value,
+    *   @return static
+    */
+    public function insert(
+        int $position,
+        mixed $value
+    //): mixed {
+    ) {
+      $array = $this->toArray();
+      return new static(
+        array_splice(
+        $array,
+        $position,
+        0,
+        $value,
+      ));
+    }
+    
+    /**
+    *   delete
+    *
+    *   @param int|string $key
+    *   @return static
+    */
+    public function delete(
+      //int|string $key
+      $key
+    //): static {
+    ) {
+      $array = $this->toArray();
+      unset($array[$key]);
+      return new static(
+        $array
+      );
+    }
+
+    
+    
+    
+}
