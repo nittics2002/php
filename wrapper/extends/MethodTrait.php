487563c2a93b537666ebf585a5a5277434fe37b6
nittics1991 <bunpabcom@gmail.com>
2021-07-23 14:58:32 +0900

    wrapper/array 拡張作成中

--- wrapper/extends/MethodTrait.php
+++ wrapper/extends/MethodTrait.php
@@ -143,65 +143,6 @@ trait DefineMethodTrait
     ): bool {
       return $this->toArray() === [];
     }
-   
-    /**
-    *  append
-    *
-    *   @param StandardArrayObject $sao
-    *   @return static
-    */
-    public function append(
-    ): static {
-      return new static(
-        $this->toArray() +
-        $sao->toArray()
-      );
-    }
-
-    /**
-    *  equals
-    *
-    *   @param StandardArrayObject $sao
-    *   @return bool
-    */
-    public function equals(
-    ): bool {
-      return $this->toArray() === $sao->toArray();
-    }
-
-    /**
-    *  notEquals
-    *
-    *   @param StandardArrayObject $sao
-    *   @return bool
-    */
-    public function notEquals(
-    ): bool {
-      return !($this->equals($sao));
-    }
-
-    /**
-    *   same 
-    *
-    *   @param StandardArrayObject $sao
-    *   @return bool
-    */
-    public function same(
-    ): bool {
-      return $this->toArray() == $sao->toArray();
-    }
-
-    /**
-    *  notSame
-    *
-    *   @param StandardArrayObject $sao
-    *   @return bool
-    */
-    public function notSame(
-    ): bool {
-      return !($this->same($sao));
-    }
-
 
 
 
